d9ef610a6642e239cc7c09514ce5dc877e5f34bc
VasuDevbyte <81460710+VasuDevbyte@users.noreply.github.com>
2021-05-05 08:29:39 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -4,7 +4,7 @@ from config import Config
 class Translation(object):
   START_TXT = """<b>Hai {}!!</b>
 <i>I'm Simple Auto file Forward Bot
-This Bot forward all files to One Public channel to Your Personal channel
+This Bot forward all files to One Public channel to Your Personal channel By VasuDev
 More details /help</i>"""
   CAPTION = "`{}`\n\n" + str(Config.CAPTION)
   HELP_TXT = """<b>Follow These Steps!!</b>
@@ -22,7 +22,7 @@ More details /help</i>"""
   ABOUT_TXT = """<b><u>My Info</b></u>
 
 <b>Name :</b> <code>Auto Forward Bot</code>
-<b>Credit :</b> <code>Dark Angel</code>
+<b>Credit :</b> <code>Vasu Dev</code>
 <b>Language :</b> <code>Python3</code>
 <b>Lybrary :</b> <code>Pyrogram v1.2.9</code>
 <b>Server :</b> <code>Heroku</code>
